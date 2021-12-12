c9a46008371fe0073c9d7ac73715dda393a94f6b
Hasan Zeki Göğebakan <hsnzkg@gmail.com>
2021-12-12 18:33:57 +0300

    1.0

---
+++ dataParser.py
@@ -0,0 +1,236 @@
+import json
+from typing import Literal
+from numpy.core.fromnumeric import size 
+import requests
+from PIL import Image,ImageDraw,ImageFont
+import enum
+import os
+import numpy as np
+from requests.api import patch
+from datetime import *
+
+coreDataPath = "coreDatabase/"
+databasePath = "databaseImages/"
+paintedDatabasePath = "paintedDatabaseImages/"
+externalDatabasePath =  "externalDatabaseImages/"
+tempDatabasePath = "tempDatabaseImages/"
+
+class colorCategory (enum.Enum):
+    COMMON = (128, 128, 128,255)
+    UNCOMMON = (0, 255, 0,255)
+    UNIQUE = (0, 153, 255,255)
+    RARE = (255, 0, 255,255)
+    EPIC = (255, 153, 0,255)
+    LEGENDARY = (255, 0, 0,255)
+    TEXT = (245,204,39,255)
+    TEXTSTROKE = (0,0,0,255)
+
+
+def GetMomoPhotoIDValue(requiredKey): 
+    with open('{path}data.txt'.format(path = coreDataPath)) as f:
+        data = f.readlines()
+        dataCleared = [element.replace('VM44219:1','') for element in data]
+        dataSecondCleared = [element.replace(' ','') for element in dataCleared]
+
+    data_set = {}
+    for i in dataSecondCleared:
+        miniData = i.split('img')
+        values = miniData[1]
+        values = values.split('.')
+        key = values[0].replace('/','')
+        value =values[1]
+        data_set[key] = value
+    
+    return  data_set[str(requiredKey)]
+
+
+def DownloadDatabaseImages():
+    from mobox import GetMomoPhotoLink
+    with open("{path}data.txt".format(path = coreDataPath)) as f:
+        data = f.readlines()
+        dataCleared = [element.replace('VM44219:1','') for element in data]
+        dataSecondCleared = [element.replace(' ','') for element in dataCleared]
+        dataThirdCleared = [element.replace('\n','') for element in dataSecondCleared]
+        dataFourCleared = [element.replace(' ','') for element in dataThirdCleared]
+        dataFiveCleared = [element.replace('â€‹','') for element in dataFourCleared]
+        for i in dataFiveCleared:
+            tempMOMOID = i.split("/")[-1].split(".")[0]
+            tempPHOTOID = i.split("/")[-1].split(".")[1]
+            tempPHOTOURL = GetMomoPhotoLink(tempMOMOID,tempPHOTOID)       
+            with open('{path}{momoID}-{photoID}.png'.format(momoID = tempMOMOID,photoID = tempPHOTOID,path = databasePath), 'wb') as handle:
+                imageResponse = requests.get(tempPHOTOURL, stream=True)
+                if not imageResponse.ok:
+                    print("NO IMAGE")
+                for block in imageResponse.iter_content(512):
+                    if not block:
+                        break
+                    handle.write(block)
+
+def PaintDatabaseImages():
+    from mobox import GetMomoRarity
+    for f in os.listdir(databasePath):
+        tempmomoID = f.split("-")[0]
+        tempPhotoID = f.split("-")[1].split(".")[0]
+        tempRarity = GetMomoRarity(tempmomoID)
+       
+
+        coreImage = Image.open('{path}{image}'.format(path = databasePath, image = f)).convert("RGBA")
+        hashrateImage = Image.open('{path}hashrateCutted.png'.format(path = externalDatabasePath)).convert("RGBA")
+        hashrateResizedImage = hashrateImage.resize((45,45))
+
+        finalPaintedImage = Image.new('RGBA',(512,512),colorCategory[tempRarity].value)
+        finalPaintedImage.paste(coreImage,(0,0),coreImage)
+        finalPaintedImage.paste(hashrateResizedImage,(10,10),hashrateResizedImage)
+
+        smallFont = ImageFont.truetype("impact.ttf",15)
+        draw = ImageDraw.Draw(finalPaintedImage)
+
+        #marka
+        draw.text(((512/2-190),(512/2 +240)),"@MOMOCATCHERBOT",colorCategory.TEXT.value, stroke_width=2,stroke_fill=colorCategory.TEXTSTROKE.value,font =smallFont,anchor="mm")
+        finalPaintedImage.save("{path}{mergedImage}.png".format(path =paintedDatabasePath, mergedImage = tempmomoID + "-" + tempPhotoID),"PNG")
+
+
+
+
+def PaintImageTexts(hashrate,price,momoID,momoPhotoID):
+
+    bigFont = ImageFont.truetype("impact.ttf",50)
+    mediumFont = ImageFont.truetype("impact.ttf",35)
+
+
+    paintedImage = Image.open('{path}{image}.png'.format(path = paintedDatabasePath, image = str(momoID) + "-" + str(momoPhotoID))).convert("RGBA")
+
+    draw = ImageDraw.Draw(paintedImage)
+
+    #busd
+    draw.text(((512/2),(512/2 + 190)),"{price} BUSD".format(price = price),colorCategory.TEXT.value, stroke_width=3,stroke_fill=colorCategory.TEXTSTROKE.value,font =bigFont,anchor="mm")
+
+    #hashrate
+    draw.text(((512/2 -175),(512/2 -225 )),"{hashrate}".format(hashrate = hashrate),colorCategory.TEXT.value, stroke_width=2,stroke_fill=colorCategory.TEXTSTROKE.value,font =mediumFont,anchor="mm")
+    
+    paintedImage.save("{path}{tempImage}.png".format(path = tempDatabasePath, tempImage = str(momoID) + "-" + str(momoPhotoID)),"PNG")
+
+
+def StopUserSets(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                i["status"] = "off"
+    with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def StartUserSets(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                i["status"] = "on"
+    with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def CheckUserSubscription(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                tempRDate = i["rDate"]
+                tempEDate = i["eDate"]
+                if(tempEDate != ""):
+                    tempYear = int(tempEDate.split("-")[0])
+                    tempMonth = int(tempEDate.split("-")[1])
+                    tempDay = int(tempEDate.split("-")[2])
+
+                    if(datetime(date.today().year, date.today().month, date.today().day) >  datetime(tempYear, tempMonth, tempDay)):
+                        return False
+                    else:
+                        return True
+
+def CheckUserDatabase(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                return True
+        return False
+
+def SaveUser(userID,userName):
+    tempUser = {}
+    tempUser["userID"] = userID
+    tempUser["userName"] = str(userName)
+    tempUser["sets"] = []
+    tempUser["rDate"] = date.today()
+    tempUser["eDate"] = ""
+    tempUser["status"] = "off"
+    isValid = False
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                isValid = True
+    if(isValid == False):          
+        data["users"].append(tempUser)
+        with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+            json.dump(data, f,default=str)
+
+
+def ClearAllDatabase():
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    data["users"].clear()
+    with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def ClearUserSets(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                i["sets"].clear()
+    with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def CheckUserSets(userID,set):
+    from mobox import DictCompare
+
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                isValid = False
+                for x in i["sets"]:
+                    if(DictCompare(x,set)):
+                        isValid = True
+                
+    return isValid
+
+
+def GetOnlineUsers():
+    tempOnlineUsers = []
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["status"] == "on"):
+                tempOnlineUsers.append(i)
+    return tempOnlineUsers
+
+
+def SaveUserSet(userID,set):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                i["sets"].append(set)
+                i["status"] = "on"
+    with open('{path}users.json'.format(path = coreDataPath), 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def GetUserSets(userID):
+    with open('{path}users.json'.format(path = coreDataPath), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for i in data["users"]:
+            if(i["userID"] == userID):
+                return i["sets"]
+
---
+++ mobox.py
@@ -0,0 +1,383 @@
+from json.encoder import py_encode_basestring
+from logging import NullHandler
+from typing import final
+from PIL.Image import RASTERIZE
+from numpy import False_
+import threading
+import requests
+import json
+import enum
+import os
+from time import sleep
+import datetime
+from requests.models import Response
+
+import telegram
+from telegram import message
+from telegram import bot
+from telegram import chat
+from telegram import parsemode
+from telegram import update
+from telegram.constants import PARSEMODE_HTML, PARSEMODE_MARKDOWN, PARSEMODE_MARKDOWN_V2
+import dataParser
+
+from telegram import ParseMode
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+from telegram.ext import Defaults
+
+
+
+
+#region ENUMS
+
+class category (enum.Enum):
+    ALL = "0"
+    WIND = "1"
+    EARTH = "2"
+    WATER = "3"
+    CREATURES = "4"
+    HUMANS = "5"
+
+class sort(enum.Enum):
+   LTACC = "-time"
+   LTDECC = "time"
+   PACC = "price"
+   PDEC = "-price"
+   QUAACC = "hashrate"
+   QUADEC = "-hashrate"
+
+class pType (enum.Enum):
+    DEFAULT = ''
+
+class vType (enum.Enum):
+    ALL = "0"
+    COMMON = "1"
+    UNCOMMON = "2"
+    UNIQUE = "3"
+    RARE = "4"
+    EPIC = "5"
+    LEGENDARY = "6"
+#endregion ENUMS
+
+
+currentvType = vType.UNIQUE
+currentCategory = category.ALL
+currentSort = sort.LTACC
+currentPType = pType.DEFAULT
+currentChain = 'BNB'
+currentPage = 1
+currentLimit = 15
+lastResponse = []
+currentTickRate = 5
+currentStartTickRate = 5
+channelID = "-1001746717168"
+botID ="5074524076:AAG9g8QsG8xb-RxSOJCAFhkb9GIRm60M1ZU"
+newBotID = "5074248859:AAHIOiI4-CnpoCX4Qb0XTqD6qbkY8x-go_Q"
+MOMOMARKETNORMALURL = 'https://www.mobox.io/home/#/iframe/momo?path=market&tab=0'
+
+
+#region TEXT MESSAGES
+         
+MOMOAUCTIONURL = 'https://nftapi.mobox.io/auction/search/{chain}?page={page}&limit={limit}&category={category}&vType={vType}&sort={sort}&pType={pType}'
+MOMOPHOTOURL = 'https://www.mobox.io/momo/img/{key}.{value}.png'
+
+SETPOSITIVETEXT = "⏳ I WILL SEND YOU A MESSAGE WHEN ANY *{rarity}* price  *{operator}* *BUSD{price}*\n"
+WELCOMETEXT = "👋WELCOME👋 *{username}*\nOUR ADMINS WILL TAKE YOUR *REGISTER* PROCESS AND YOU WILL BE NOTIFIED *SOON*...\n*YOUR ID*: *{id}*"
+MOMOREPLYTEXT = "🔥*NEW MOMO LISTED*🔥\nMOMO PRICE: *{price} BUSD*\nMOMO HASHRATE: *{hashrate}*\nMOMO LEVEL: *{level}*\nMOMO RARITY: *{rarity}*\n"
+STARTTEXT = "*👋Welcome MOMO Catcher👋*\nThis bot can useful for catching *CHEAP MOMO's* in MOMO market before *EVERYONE*🔥\n\nYou can find bot *command usages* in */help*"
+
+STOPPROCESSTEXT = "⚠️STOPPING ALL */SET* PROCESS..."
+STARTPROCESSTEXT = "⚠️STARTING ALL */SET* PROCESS..."
+CLEARPROCESSTEXT = "⚠️CLEARING ALL */SET* PROCESS..."
+
+REGISTERNEGATIVETEXT = "⚠️PLEASE PROVIDE A VALID */REGISTER* USAGE: \n FOR EXAMPLES PLEASE USE */help*"
+REGISTERSECONDNEGATIVETEXT = "YOU ARE *ALREADY* REGISTERED !"
+
+HELPTEXT = "For */set* required MOMO price range usage: */set* \n\n📗*OPERATOR EXAMPLES*📕\n\n*RARITY* : *COMMON | UNCOMMON | UNIQUE | RARE | EPIC | LEGENDARY*\n\n*OPERATOR* : * > <*\n\n*AMOUNT BUSD* : *100.2*\n\n *Example Set Usage* : */set |RARITY| |OPERATOR| |AMOUNTBUSD|*"
+SETNEGATIVETEXT = "⚠️PLEASE *REGISTER* FIRST !\nFOR EXAMPLES PLEASE USE */help*"
+SETSECONDNEGATIVETEXT = "⚠️YOUR MEMBERSHIP IS *EXPIRED* OR *NOT ACTIVATED*!\n PLEASE CONTACT ANY ADMIN VIA OUR GROUP : *https://t.me/MOMOCATCHER*"
+SETTHIRDNEGATIVETEXT = "⚠️PLEASE PROVIDE A VALID */SET* USAGE: \n FOR EXAMPLES PLEASE USE */help*"
+SETFOURTHNEGATIVETEXT = "⚠️YOU ALREADY SETTED THIS *SET* !*/SET* USAGE: \n FOR EXAMPLES PLEASE USE */help*"
+
+UNKOWNTEXT = "⚠️You typed wrong *COMMAND* for help please use -> */help*"
+
+#endregion TEXTMESSAGES
+
+headers = {
+    'Content-Security-Policy': 'upgrade-insecure-requests',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    'authority': 'bsc-dataseed2.binance.org',
+    'scheme': 'https',
+    'referrer': 'https://www.mobox.io/',
+    'sec-ch-ua-platform': 'Windows',
+    'referer':'https://www.mobox.io'
+    }
+
+
+
+
+
+def GetMomoPhotoID(MOMOID):
+    return dataParser.GetMomoPhotoIDValue(MOMOID)
+
+def GetMomoPhotoLink(ID,PHOTOID):
+    return MOMOPHOTOURL.format(key = str(ID), value = str(PHOTOID))
+
+def GetMomoName(momoJson):
+    #TODO
+    return
+
+def GetLastMomos(chain,page,limit,category,vType,sort):
+    requestURL = MOMOAUCTIONURL.format(chain = chain, page = page , limit = limit,category = category,vType = vType,sort = sort, pType = currentPType)
+    response = requests.get(requestURL,headers=headers)
+    json_data = json.loads(response.content)
+    for momojson in json_data["list"]:
+        for key in momojson.keys():
+            if(type(momojson[key]) == list):
+                momojson[key] = tuple(momojson[key])
+    return json_data["list"]
+
+def GetMomoID(momoJson):
+    if(momoJson):
+        if(momoJson["prototype"]): 
+            return momoJson["prototype"]
+    else:   
+        return "NO  DATA"
+
+def GetMomoPrice(momoJson):
+    if(momoJson):
+        if("startPrice" in momoJson):
+            return momoJson["startPrice"]/1000000000
+        elif("endPrice" in momoJson): 
+            return momoJson["endPrice"]/1000000000
+        else:
+            return momoJson["nowPrice"]/1000000000
+    else:
+        return("NO  DATA")
+
+def GetMomoHashRate(momoJson):
+    if(momoJson):
+        if("lvHashrate" in momoJson):
+            return momoJson["lvHashrate"]
+        elif("hashrate" in momoJson): 
+            return momoJson["hashrate"]
+    else:
+        return("NO  DATA")
+
+def GetMomoRarity(momoID):
+    tempMOMOID = momoID
+    tempFirstIDLetter = str(tempMOMOID)[0]
+    return  str(vType(tempFirstIDLetter).name)
+
+def GetMomoLevel(momoJson):
+    if(momoJson):
+        if("level" in momoJson):
+            return momoJson["level"]
+    else:
+        return("NO  DATA")
+
+def GetMomoAllSpecs(momoJson):
+    if(momoJson):
+        specs = {}
+        specs["momoID"] = GetMomoID(momoJson)
+        specs["photoID"] = GetMomoPhotoID(GetMomoID(momoJson))
+        specs["price"] = GetMomoPrice(momoJson)
+        specs["level"] = GetMomoLevel(momoJson)
+        specs["hashrate"] = GetMomoHashRate(momoJson)
+        specs["photoLink"] = GetMomoPhotoLink(GetMomoID(momoJson),GetMomoPhotoID(GetMomoID(momoJson)))
+        specs["rarity"] = GetMomoRarity(GetMomoID(momoJson))
+        return specs
+    else:
+        return("NO  DATA")
+
+def GetAnnounceText(momoJson):
+    return MOMOREPLYTEXT.format(price = momoJson["price"],hashrate = momoJson["hashrate"], level = momoJson["level"], rarity = momoJson["rarity"])
+     
+def startCommand(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=STARTTEXT,parse_mode = PARSEMODE_MARKDOWN)
+
+def helpCommand(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=HELPTEXT,parse_mode = PARSEMODE_MARKDOWN)
+
+def stopBotCommand(update,context):
+    response = STOPPROCESSTEXT
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response,parse_mode = PARSEMODE_MARKDOWN)
+    dataParser.StopUserSets(update.effective_chat.id)
+
+def startBotCommand(update,context):
+    response = STARTPROCESSTEXT
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response,parse_mode = PARSEMODE_MARKDOWN)
+    dataParser.StartUserSets(update.effective_chat.id)
+
+def registerCommand(update,context):
+    if(len(context.args) > 0 and len(context.args ) < 2):
+        if(dataParser.CheckUserDatabase(update.effective_chat.id)):
+            response = REGISTERSECONDNEGATIVETEXT
+        else:
+            dataParser.SaveUser(update.effective_chat.id,str(context.args[0]))
+            response = WELCOMETEXT.format(username = str(context.args[0]),id= str(update.effective_chat.id))       
+    else:
+        response = REGISTERNEGATIVETEXT
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response,parse_mode = PARSEMODE_MARKDOWN)
+
+def setCommand(update, context):
+    if  len(context.args) > 2 and len(context.args) < 4:
+        boolOne = has_value(vType,context.args[0].upper())
+        boolTwo = (str(context.args[1])) == '<' or (str(context.args[1])) == '>'
+        boolThree = is_float(context.args[2])
+
+        if  is_allTrue((boolOne,boolTwo,boolThree)):
+            if dataParser.CheckUserDatabase(update.effective_chat.id) and dataParser.CheckUserSubscription(update.effective_chat.id):
+                tempUserID = update.effective_chat.id
+                tempRare = context.args[0].upper()
+                tempOperator = context.args[1]
+                tempPrice = context.args[2]
+
+                tempSet = {
+                    "RARITY":tempRare,
+                    "OPERATOR":tempOperator,
+                    "PRICE":tempPrice
+                }
+                if(dataParser.CheckUserSets(tempUserID,tempSet)):
+                    response = SETFOURTHNEGATIVETEXT.format(rarity = tempRare,operator = tempOperator , price = tempPrice)
+                else:
+                    dataParser.SaveUserSet(tempUserID,tempSet)
+                    response = SETPOSITIVETEXT.format(rarity = tempRare,operator = tempOperator , price = tempPrice)
+            else:
+                if(dataParser.CheckUserDatabase(update.effective_chat.id) == False):
+                    response = SETNEGATIVETEXT
+                else:
+                    response = SETSECONDNEGATIVETEXT
+        else:
+            response = SETTHIRDNEGATIVETEXT 
+               
+    else:
+        response = SETTHIRDNEGATIVETEXT
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response,parse_mode = PARSEMODE_MARKDOWN)
+
+def has_value(cls, value):
+    return value in cls._member_names_
+
+def is_float(element: any) -> bool:
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+def is_allTrue(tuple):
+    for i in tuple:
+        if(i == False):
+            return False
+    return True
+
+def setCallback(): 
+    global lastResponse 
+    sendables = []
+
+    if(len(dataParser.GetOnlineUsers()) > 0):   
+        response = GetLastMomos(currentChain,currentPage,currentLimit,currentCategory,currentvType,currentSort)
+        if(len(response) > 0):
+
+            if(len(lastResponse) == 0):
+                lastResponse = response
+                sendables = lastResponse
+            else:
+                for newJSON in (response):
+                    isAddeable = True
+                    for oldJSON in (lastResponse):
+                        if(DictCompare(newJSON,oldJSON) == True):
+                            isAddeable = False
+                    if(isAddeable == True):
+                        sendables.append(newJSON)
+                lastResponse = response
+
+
+            
+           
+
+
+    if(len(sendables) > 0):        
+        for x in sendables:
+            send = False
+            tempSpecs = GetMomoAllSpecs(x)
+            tempCurrentRarity = tempSpecs["rarity"]
+            tempCurrentPrice = tempSpecs["price"]
+
+
+            for user in dataParser.GetOnlineUsers():
+                if(len(dataParser.GetUserSets(user["userID"]))> 0):
+                    for i in dataParser.GetUserSets(user["userID"]):
+                        if i["OPERATOR"] == '<' and tempCurrentRarity == i["RARITY"]:
+                            if float(i["PRICE"]) >= float(tempCurrentPrice):
+                                send = True
+                        else:
+                            if float(i["PRICE"]) <= float(tempCurrentPrice)  and tempCurrentRarity == i["RARITY"]:
+                                send = True
+                    if(send):
+                        dataParser.PaintImageTexts(tempSpecs["hashrate"],tempSpecs["price"],tempSpecs["momoID"],tempSpecs["photoID"])
+                        updater.bot.sendPhoto(chat_id=user["userID"], photo = open('{path}{momoID}-{photoID}.png'.format(path = dataParser.tempDatabasePath,momoID = tempSpecs["momoID"],photoID = tempSpecs["photoID"]), 'rb'),caption = "{}".format(GetAnnounceText(tempSpecs)),parse_mode = PARSEMODE_MARKDOWN)
+                        os.remove("{path}{momoID}-{photoID}.png".format(path = dataParser.tempDatabasePath, momoID = tempSpecs["momoID"],photoID = tempSpecs["photoID"]))
+
+    threading.Timer(currentTickRate, setCallback).start()
+
+
+def clearBotCommand(update,context):
+    dataParser.ClearUserSets(update.effective_chat.id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=CLEARPROCESSTEXT,parse_mode = PARSEMODE_MARKDOWN)
+
+def BotSession():
+    sleep(currentStartTickRate)
+    threading.Timer(currentTickRate, setCallback).start()
+    global updater
+    updater = Updater(newBotID,use_context=True)
+
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start',startCommand))
+    dispatcher.add_handler(CommandHandler('help',helpCommand))
+    dispatcher.add_handler(CommandHandler('register',registerCommand))
+    dispatcher.add_handler(CommandHandler('set',setCommand))
+    dispatcher.add_handler(CommandHandler('stopbot',stopBotCommand))
+    dispatcher.add_handler(CommandHandler('startbot',startBotCommand))
+    dispatcher.add_handler(CommandHandler('clearbot',clearBotCommand))
+
+
+    updater.start_polling()
+    updater.idle() 
+
+def DictCompare(d1, d2):
+    d1_values = set(d1.values())
+    d2_values = set(d2.values())
+
+
+    sameValues = len(d1_values.intersection(d2_values))
+
+
+    if(sameValues == len(d1_values) and sameValues == len(d2_values)):
+        return True
+
+    else:
+        return False
+
+def ChannelSession():
+    updater = Updater(newBotID,use_context=True)
+    lastMomosJSON = GetLastMomos(currentChain,currentPage,currentLimit,currentCategory,currentvType,currentSort)
+    lastMomoJSONDatas = GetMomoAllSpecs(lastMomosJSON[0])
+    dataParser.PaintImageTexts(lastMomoJSONDatas["hashrate"],lastMomoJSONDatas["price"],lastMomoJSONDatas["momoID"],lastMomoJSONDatas["photoID"])
+    updater.bot.sendPhoto(chat_id='@momotracker',photo =open('{path}{momoID}-{photoID}.png'.format(path = dataParser.tempDatabasePath,momoID = lastMomoJSONDatas["momoID"],photoID = lastMomoJSONDatas["photoID"]), 'rb'),caption = "{}".format(GetAnnounceText(lastMomoJSONDatas)),parse_mode = PARSEMODE_MARKDOWN)
+    os.remove("{path}{momoID}-{photoID}.png".format(path = dataParser.tempDatabasePath, momoID = lastMomoJSONDatas["momoID"],photoID = lastMomoJSONDatas["photoID"]))
+
+def DatabaseSession():      
+    dataParser.DownloadDatabaseImages()
+    dataParser.PaintDatabaseImages()
+
+if __name__ == '__main__':
+    #dataParser.PaintDatabaseImages()
+    #dataParser.DownloadDatabaseImages()  
+    BotSession()
